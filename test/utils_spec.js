b127b8bbf2348733ea6d880d2475944365de0aa3
jake hoffner <jake.hoffner@gmail.com>
2014-06-12 16:41:21 -0700

    - docker file now includes rspec, mini test, mocha and chai. It no longer sets "run" as an entry point. - utilizing dockerode package to utilize remote api for running instead of using cli

--- test/utils_spec.js
+++ test/utils_spec.js
@@ -1,6 +1,7 @@
 
 var expect = require('chai').expect;
 var parseBodyToArgs = require('../lib/util').parseBodyToArgs;
+var parseBodyToArray = require('../lib/util').parseBodyToArray;
 
 
 describe( 'parseBodyToArgs', function(){
@@ -18,5 +19,30 @@ describe( 'parseBodyToArgs', function(){
 
         expect(args ).to.contain('-l "ruby"');
         expect(args ).to.contain(' --solution "solution \\"a\\"');
+    });
+
+});describe( 'parseBodyToArray', function(){
+    it( 'should parse body into command line args array', function() {
+        var body = {l: 'ruby', c: 'solution'}
+        var args = parseBodyToArray(body);
+
+
+        expect(args[0]).to.equal('-l')
+        expect(args[1]).to.equal('ruby');
+        expect(args[3]).to.equal('solution');
+    });
+
+    it( 'should parse body with mix shortcut/full name params', function() {
+        var body = {l: 'ruby', solution: 'solution "a"'};
+        var args = parseBodyToArray(body);
+
+        expect(args[2]).to.equal('--solution');
+    });
+
+    it( 'should support ignore values', function() {
+        var body = {l: 'ruby', solution: 'solution "a"'};
+        var args = parseBodyToArray(body, ['solution']);
+
+        expect(args.length).to.equal(2);
     });
 });
