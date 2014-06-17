acfcec9a07072792759cc1348fe1d9ff75800f25
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-16 20:36:26 -0700

    Started writing the test fixture for python
    Added code to runners and supporting files to load test fixture.

---
+++ frameworks/python/cw-2.py
@@ -0,0 +1,17 @@
+
+
+class Test:
+  @staticmethod
+  def expect(passed = None, message = None):
+    if passed:
+      success_msg = "Test Passed"
+      #TODO add optional message
+
+      print "<PASSED::>" + success_msg
+    else:
+      message = message or "Value is not what was expected"
+      print "<FAILED::>" + message
+
+      raise StandardError(message)
+      #TODO describing stack maybe
+
