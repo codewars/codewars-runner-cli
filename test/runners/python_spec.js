079fa6ac4b97deec76205014080d9018a86d41a3
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-17 14:16:53 -0700

    Added support for python unittest framework

--- test/runners/python_spec.js
+++ test/runners/python_spec.js
@@ -11,14 +11,14 @@ describe( 'python runner', function(){
             });
         });
     });
-    describe('cw-2', function(){
+    describe( 'cw-2', function(){
         it( 'should handle a basic assertion', function(done){
             runner.run({language: 'python',
                         solution: 'a = 1',
                         fixture: 'Test.expect(a == 1)',
                         testFramework: 'cw-2'},
-                          function(buffer){
-                console.log(buffer)
+                       function(buffer){
+                console.log(buffer);
                 expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
                 done();
             });
@@ -28,11 +28,56 @@ describe( 'python runner', function(){
                         solution: 'a = 1',
                         fixture: 'Test.expect(a == 2)',
                         testFramework: 'cw-2'},
-                          function(buffer){
-                console.log(buffer)
+                       function(buffer){
+                console.log(buffer);
                 expect(buffer.stdout).to.equal('<FAILED::>Value is not what was expected\n');
                 done();
             });
         });
     });
+    describe( 'unittest', function(){
+        it( 'should handle a basic assertion', function(done){
+            runner.run({language: 'python',
+                        solution: 'a = 1',
+                        fixture: 'class TestSequenceFunctions(unittest.TestCase):\n'
+                               + '  def test_assert(self):\n'
+                               + '    self.assertEqual(a, 1)\n'
+                               + '_testsuite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)',
+                        testFramework: 'unittest'},
+                       function(buffer){
+                console.log(buffer);
+                expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
+                done();
+            });
+        });
+        it( 'should handle a failed assetion', function(done){
+            runner.run({language: 'python',
+                        solution: 'a = 1',
+                        fixture: 'class TestSequenceFunctions(unittest.TestCase):\n'
+                               + '  def test_assert(self):\n'
+                               + '    self.assertEqual(a, 2, "test failed")\n'
+                               + '_testsuite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)',
+                        testFramework: 'unittest'},
+                       function(buffer){
+                console.log(buffer);
+                expect(buffer.stdout).to.equal('<FAILED::>test failed\n');
+                done();
+            });
+        });
+        it( 'should handle a failed assetion', function(done){
+            runner.run({language: 'python',
+                        solution: 'a = 1',
+                        fixture: 'class TestSequenceFunctions(unittest.TestCase):\n'
+                               + '  def test_assert(self):\n'
+                               + '    raise StandardError("exception")\n'
+                               + '_testsuite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)',
+                        testFramework: 'unittest'},
+                       function(buffer){
+                console.log(buffer);
+                expect(buffer.stdout).to.equal('<FAILED::>Unhandled Exception: exception\n');
+                done();
+            });
+        });
+    });
+
 });
