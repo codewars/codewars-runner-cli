a1ac37d75c9625ce8cdb3c88dbc78319ced4a613
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-19 14:12:36 -0700

    Initial implementation of rspec test support

---
+++ frameworks/ruby/cwrspecformatter.rb
@@ -0,0 +1,24 @@
+class CwRSpecFormatter
+  def initialize(output)
+    @output = output
+  end
+  def formatMessage(s)
+    s.gsub("\n", "\\n")
+  end
+  RSpec::Core::Formatters.register self, :example_group_started, :example_started, :example_passed, :example_failed, :message
+  def example_group_started(notification)
+    @output.puts "<DESCRIBE::>" + formatMessage(notification.group.description)
+  end
+  def example_started(notification)
+    @output.puts "<IT::>" + formatMessage(notification.example.description)
+  end
+  def example_passed(notification)
+    @output.puts "<PASSED::>" + formatMessage(notification.example.description)
+  end
+  def example_failed(notification)
+    @output.puts "<FAILED::>" + formatMessage(notification.exception.message) + "\\n" + notification.formatted_backtrace.join("\\n")
+  end
+  def message(notification)
+    @output.puts formatMessage(notification.message)
+  end
+end
