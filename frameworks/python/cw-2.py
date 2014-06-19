a3669d6da043038469d79893068ff3133b8a46b5
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-19 16:06:24 -0700

    Updated python cw2 test fixture, adjusted unittest

--- frameworks/python/cw-2.py
+++ frameworks/python/cw-2.py
@@ -1,6 +1,10 @@
-
+import string
 
 class Test:
+  @staticmethod
+  def formatMessage(message):
+      return string.replace(message, "\n", "\\n")
+
   @staticmethod
   def expect(passed = None, message = None):
     if passed:
@@ -15,4 +19,37 @@ class Test:
       raise StandardError(message)
       #TODO describing stack maybe
 
+  @staticmethod
+  def assert_equals(actual, expected, message = None):
+    expect(actual == expected, message)
+
+  @staticmethod
+  def assert_not_equals(actual, expected, message = None):
+    expect(actual != expected, message)
+
+  def expect_error(message, function):
+    passed = false
+    try:
+      function()
+    except Exception:
+      passed = true
+
+    expect(passed, message)
+
+  def expect_error(message, function):
+    passed = true
+    try:
+      function()
+    except Exception:
+      passed = false
+
+    expect(passed, message)
+
+  @staticmethod
+  def describe(message):
+    print "<DESCRIBE::>" + formatMessage(message)
+
+  @staticmethod
+  def it(message):
+    print "<IT::>" + formatMessage(message)
 
