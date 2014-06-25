b243c304917e3cc282afcd3a85bf06cf1e5a3a7c
jake hoffner <jake.hoffner@gmail.com>
2014-06-25 12:36:18 -0700

    updated scripts and added additional scripts for deployment

--- setup/provision.sh
+++ setup/provision.sh
@@ -5,14 +5,10 @@ apt-get -y update
 echo "INSTALL SysOp Tools"
 apt-get install -y iptables
 
-
 apt-get install monit
 
 # Add nodejs repo
 add-apt-repository -y ppa:chris-lea/node.js
-
-echo "INSTALL MONO SHELL"
-apt-get -y install mono-csharp-shell --fix-missing
 
 echo "INSTALL NODEJS"
 apt-get install -y nodejs
@@ -20,17 +16,14 @@ apt-get install -y nodejs
 echo "INSTALL NPM"
 apt-get -qq -y install npm
 
-echo "Install supervisor"
-npm install -g supervisor
-
 echo "Install server packages"
 cd /vagrant
 npm install
-
-npm install -g mocha
 
 echo "Setting some sandbox limits"
 sudo iptables -A OUTPUT -m owner --uid-owner 1000 -j DROP
 sudo sed -i '$a vagrant soft nproc 20' /etc/security/limits.conf
 sudo sed -i '$a session required pam_limits.so' /etc/pam.d/common-session
+
+
 
