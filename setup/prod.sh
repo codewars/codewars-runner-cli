033c5e48945e00d2aac3f81e0bd2a67a4476093c
jake hoffner <jake.hoffner@gmail.com>
2014-06-25 12:37:29 -0700

    took git clone out of script, which needs to be done before the script can even exist... duh.

--- setup/prod.sh
+++ setup/prod.sh
@@ -3,7 +3,6 @@
 sudo npm install pm2 -g
 
 cd /
-git clone https://github.com/codewars/codewars-runner
 sudo cp /codewars-runner/setup/codewars-runner.conf /etc/init/codewars-runner.conf
 
 cd /codewars-runner
