18ee91a10b69f7c46ff1fcc5baaefd8bf7928084
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-26 16:21:41 -0700

    Added more setup scripts for faster deployment

--- setup/provision.sh
+++ setup/provision.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "APT-GET UPDATE"
 apt-get -y update
 
@@ -13,8 +14,14 @@ add-apt-repository -y ppa:chris-lea/node.js
 echo "INSTALL NODEJS"
 apt-get install -y nodejs
 
+echo "Symlinking nodejs -> node"
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
 echo "INSTALL NPM"
 apt-get -qq -y install npm
+
+echo "Installing pm2"
+sudo npm install pm2 -g
 
 echo "Install server packages"
 cd /vagrant
@@ -24,6 +31,3 @@ echo "Setting some sandbox limits"
 sudo iptables -A OUTPUT -m owner --uid-owner 1000 -j DROP
 sudo sed -i '$a vagrant soft nproc 20' /etc/security/limits.conf
 sudo sed -i '$a session required pam_limits.so' /etc/pam.d/common-session
-
-
-
