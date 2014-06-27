5bc9e67897b924d5cca95ffb7899227e49822eb6
jake hoffner <jake.hoffner@gmail.com>
2014-06-26 19:47:46 -0700

    version bump, tweaked setup scripts

--- setup/dev.sh
+++ setup/dev.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "Install server packages"
+cd /vagrant
+npm install
+
 echo "INSTALL MONO SHELL"
 apt-get -y install mono-csharp-shell --fix-missing
-
-cd /vagrant
 
 echo "Install supervisor"
 sudo npm install supervisor -g
