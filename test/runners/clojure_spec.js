a3c09f0e98e8a8052b013031ac1e4bd824ea8ac8
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-07 01:42:17 -0500

    Introducing Clojure runner/test-suite

---
+++ test/runners/clojure_spec.js
@@ -0,0 +1,106 @@
+var expect = require('chai').expect;
+var runner = require('../../lib/runners/clojure');
+
+
+describe('clojure runner', function () {
+    describe('.run', function () {
+        it('should handle basic code evaluation', function (done) {
+            runner.run({language: 'clojure',
+                solution: '(println "42")'
+            }, function (buffer) {
+                expect(buffer.stdout).to.equal('42\n');
+                done();
+            });
+        });
+        it('should handle running a namespace with imports', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: [
+                    '(ns foo (:require [clojure.edn :as edn]))',
+                    '(print (get (edn/read-string "{:code \\"wars\\"}") :code))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.equal('wars');
+                done();
+            });
+        });
+        it('should handle setup code', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: [
+                    '(ns foo.solution-code (:require [foo.fighters]))',
+                    '(foo.fighters/everlong)'
+                ].join('\n'),
+                setup: [
+                    '(ns foo.fighters)',
+                    '(defn everlong [] (print "Hello, I\'ve waited here for you"))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.equal('Hello, I\'ve waited here for you');
+                done();
+            });
+        });
+    });
+    describe('codewars test framework (clojure.test)', function () {
+        it('should be able to run a basic test', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: '(ns empty.namespace)',
+                fixture: [
+                    '(ns clojure.test.example (:use clojure.test))',
+                    '(deftest add-1-to-1 (testing "arithmetic works" (is (= 2 (+ 1 1)))))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.contain('<DESCRIBE::>add-1-to-1');
+                expect(buffer.stdout).to.contain('<IT::>arithmetic works');
+                expect(buffer.stdout).to.contain('<PASSED::>Test Passed\n');
+                done();
+            });
+        });
+        it('should be able to fail', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: '(ns empty.namespace)',
+                fixture: [
+                    '(ns clojure.test.example (:use clojure.test))',
+                    '(deftest sad-path (testing "won\'t work" (is (= 2 1) "bad math")))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.contain('<DESCRIBE::>sad-path');
+                expect(buffer.stdout).to.contain('<IT::>won\'t work');
+                expect(buffer.stdout).to.contain('<FAILED::>bad math - expected: (= 2 1) actual: (not (= 2 1))\n');
+                done();
+            });
+        });
+        it('should print', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: '(ns foo) (defn bar [] (print "yolo") 1)',
+                fixture: [
+                    '(ns clojure.test.example (:use clojure.test) (:require [foo]))',
+                    '(deftest printing (testing "foo/bar" (is (= 1 (foo/bar)))))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.contain('<DESCRIBE::>printing');
+                expect(buffer.stdout).to.contain('yolo<IT::>foo/bar');
+                expect(buffer.stdout).to.contain('<PASSED::>Test Passed\n');
+                done();
+            });
+        });
+        it('should have an error when there\'s an exception', function (done) {
+            runner.run({
+                language: 'clojure',
+                solution: '(ns foo)',
+                fixture: [
+                    '(ns clojure.test.example (:use clojure.test))',
+                    '(deftest exception (testing "1 / 0" (is (= 1 (/ 1 0)))))'
+                ].join('\n')
+            }, function (buffer) {
+                expect(buffer.stdout).to.contain('<DESCRIBE::>exception');
+                expect(buffer.stdout).to.contain('<IT::>1 / 0');
+                expect(buffer.stdout).to.contain('<ERROR::>expected: (= 1 (/ 1 0)) actual: \njava.lang.ArithmeticException');
+                done();
+            });
+        });
+    });
+});
