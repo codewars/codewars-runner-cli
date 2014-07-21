7314face06ffcffe877d54c6158adef785b1edcf
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-21 15:56:52 -0600

    Simple changes - Better reporting (for unit testing unit testing) - Factored out System/exit call (for with-redefs mock-out)

--- jvm-runner/src/clojure/codewars/clojure/test.clj
+++ jvm-runner/src/clojure/codewars/clojure/test.clj
@@ -14,7 +14,7 @@
 
 (defn- print-context []
   (when (seq *testing-contexts*)
-      (->> (testing-contexts-str) (str "<IT::>") println)))
+    (->> (testing-contexts-str) (str "<IT::>") println)))
 
 (defn- print-with-message [status {:keys [:message]}]
   (if (string? message)
@@ -30,28 +30,33 @@
 (defn- print-expectations [{:keys [:expected :actual]}]
   (println "expected:" (pr-str expected) "- actual:" (expr-str actual)))
 
+(defn- fail []
+  (flush)
+  (System/exit 1))
+
 (defmulti codewars-report :type)
 
 (defmethod codewars-report :pass [_]
   (with-test-out
+    (inc-report-counter :pass)
     (print-context)
     (println "<PASSED::>Test Passed")))
 
 (defmethod codewars-report :fail [m]
   (with-test-out
+    (inc-report-counter :fail)
     (print-context)
     (print-with-message "<FAILED::>" m)
     (print-expectations m))
-  (flush)
-  (System/exit 1))
+  (fail))
 
 (defmethod codewars-report :error [m]
   (with-test-out
+    (inc-report-counter :error)
     (print-context)
     (print-with-message "<ERROR::>" m)
     (print-expectations m))
-  (flush)
-  (System/exit 1))
+  (fail))
 
 (defmethod codewars-report :begin-test-ns [_])
 (defmethod codewars-report :end-test-ns [_])
