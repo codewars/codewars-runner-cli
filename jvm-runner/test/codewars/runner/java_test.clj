3210070d7624cea6a88a184ee086e380bac97c10
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-21 22:38:27 -0600

    Redirecting System.out to a string for testing purposes

--- jvm-runner/test/codewars/runner/java_test.clj
+++ jvm-runner/test/codewars/runner/java_test.clj
@@ -2,7 +2,17 @@
   (:require [clojure.test :refer :all]
             [codewars.core :refer [-main]]
             [cheshire.core :as json]
-            [codewars.runner :refer [run]]))
+            [codewars.runner :refer [run]])
+  (:import [org.apache.commons.io.output WriterOutputStream]
+           [java.io PrintStream ByteArrayOutputStream]))
+
+(defmacro with-java-out-str [& body]
+  `(let [original-out# (PrintStream. (. System out))
+         byte-out# (ByteArrayOutputStream.)]
+     (-> byte-out# (PrintStream.) System/setOut)
+     ~@body
+     (System/setOut original-out#)
+     (.toString byte-out#)))
 
 (deftest java-basic
   (testing "-main can handle a very basic java solution and fixture"
@@ -20,3 +30,12 @@
        {:language "java"
         :solution "public class FooFighters {public static int main() {return 1;}}"})
       (is (= 1 (-main))))))
+
+
+(deftest java-solution-print
+  (testing "-main can handle a java solution that prints to standard out"
+    (with-in-str
+      (json/generate-string
+       {:language "java"
+        :solution "public class Hello {public static void main() {System.out.print(\"Hellooo!\");}}"})
+      (is (= "Hellooo!" (with-java-out-str (-main)))))))
