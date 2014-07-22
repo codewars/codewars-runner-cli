0beeb65b429d7598f4449f5146402aeded5e19f8
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-22 10:16:04 -0600

    Making slightly more idiomatic

--- jvm-runner/test/codewars/test/utils.clj
+++ jvm-runner/test/codewars/test/utils.clj
@@ -5,7 +5,7 @@
 (defmacro with-java-out-str
   "A version of clojure.core/with-out-str suitable for wrapping java's System.out"
   [& body]
-  `(let [original-out# (PrintStream. (. System out))
+  `(let [original-out# (PrintStream. System/out)
          byte-out# (ByteArrayOutputStream.)]
      (-> byte-out# (PrintStream.) System/setOut)
      ~@body
