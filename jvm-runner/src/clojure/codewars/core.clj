9e515f41bb58a0a2bbff5917e487e777700cfa95
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-22 09:30:45 -0600

    Introducing with-timeout to kill processes after specified number of msecs

--- jvm-runner/src/clojure/codewars/core.clj
+++ jvm-runner/src/clojure/codewars/core.clj
@@ -1,11 +1,17 @@
 (ns codewars.core
   (:require [cheshire.core :as json]
             [codewars.runner :refer [run]]
+            [codewars.kill-switch :refer [with-timeout]]
+            [environ.core :refer [env]]
             [codewars.runner.java]
             [codewars.runner.clojure])
+  (:import [java.util.concurrent TimeoutException])
   (:gen-class))
 
 (defn -main
   "Listens to *in* for a JSON message, parses it and calls the appropriate runner"
   []
-  (-> *in* (json/parse-stream true) run))
+  (let [t (-> env :timeout Integer/parseInt)
+        input (json/parse-stream *in* true)]
+    ;; TODO: handle exceptions and run System/exit
+      (with-timeout t (run input))))
