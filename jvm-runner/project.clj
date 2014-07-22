fbef6a4970f02f289d8ae991c0ac03cdc7b21197
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-22 09:25:13 -0600

    Introducing environ

--- jvm-runner/project.clj
+++ jvm-runner/project.clj
@@ -5,7 +5,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [org.tcrawley/dynapath "0.2.3"]
-                 [junit/junit "4.11"]]
+                 [junit/junit "4.11"]
+                 [environ "0.5.0"]]
+  :plugins [[lein-environ "0.5.0"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
+  :env {:timeout "2000"}
   :main codewars.core)
