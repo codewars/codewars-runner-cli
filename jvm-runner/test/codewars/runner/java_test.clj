8ec119ac24e324776e0b1e6ac67edf93c477dc1d
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-22 11:31:16 -0600

    More test coverage

--- jvm-runner/test/codewars/runner/java_test.clj
+++ jvm-runner/test/codewars/runner/java_test.clj
@@ -30,6 +30,19 @@
         :solution "public class Hello {public static void main() {System.out.print(\"Hellooo!\");}}"})
       (is (= "Hellooo!" (with-java-out-str (-main)))))))
 
+(deftest java-setup-code
+  (testing "-main can handle a java solution and setup code"
+    (with-in-str
+      (json/generate-string
+       {:language "java"
+        :setup "import java.lang.String;
+                public class Beatles {public static String sayHello() {
+                  return \"Hello, hello!\";}}"
+        :solution "public class HelloAgain {
+                     public static void main() {
+                         System.out.print(Beatles.sayHello());}}"})
+      (is (= "Hello, hello!" (with-java-out-str (-main)))))))
+
 (deftest java-test-fixture
   (testing "-main can handle a junit test"
     (with-in-str
@@ -49,5 +62,53 @@
                          System.out.println(\"test out\");}}"})
       (let [test-out-string (with-java-out-str (-main))]
         (is (.contains test-out-string "<DESCRIBE::>myTestFunction(TestFixture)"))
+        (is (.contains test-out-string "test out"))
+        (is (.contains test-out-string "<PASSED::>Test Passed<:LF:>"))))))
+
+(deftest java-sad-path
+  (testing "-main can handle a failing junit test"
+    (with-in-str
+      (json/generate-string
+       {:language "java"
+        :solution "public class Foo {
+                     public Foo(){}
+                     public int testthing(){return 3;}
+                     public static void main(String[] args){System.out.println(\"42\");}}"
+        :fixture "import static org.junit.Assert.assertEquals;
+                  import org.junit.Test;
+                  import org.junit.runners.JUnit4;
+                  public class TestForFailure {
+                    public TestForFailure(){}
+                    @Test public void sadPath(){
+                      Foo s = new Foo();
+                      assertEquals(\"Failed Message\", 5, s.testthing());
+                      System.out.println(\"Shouldn't get here\");}}"})
+      (let [test-out-string (with-java-out-str (-main))]
+        (is (.contains test-out-string "<DESCRIBE::>sadPath(TestForFailure)<:LF:>"))
+        (is (.contains test-out-string "<FAILED::>Failed Message expected:"))
+        (is (.contains test-out-string "<5> but was:<3>"))
+        (is (not (.contains test-out-string "Shouldn't get here")))
+        (is (not (.contains test-out-string "<PASSED::>Test Passed<:LF:>")))))))
+
+(deftest java-nine-yards
+  (testing "-main can setup, solution, and test fixture code for java"
+    (with-in-str
+      (json/generate-string
+       {:language "java"
+        :setup "public class Setupp { public static int three() {return 3;}}"
+        :solution "public class Sollution {
+                   public Sollution(){}
+                   public int testthingy(){return Setupp.three();}}",
+        :fixture "import static org.junit.Assert.assertEquals;
+                  import org.junit.Test;
+                  import org.junit.runners.JUnit4;
+                  public class NineYards {
+                     public NineYards(){}
+                     @Test public void solutionAndSetupAndFixture(){
+                         Sollution s = new Sollution();
+                         assertEquals(\"wow\", 3, s.testthingy());
+                         System.out.println(\"test out\");}}"})
+      (let [test-out-string (with-java-out-str (-main))]
+        (is (.contains test-out-string "<DESCRIBE::>solutionAndSetupAndFixture(NineYards)<:LF:>"))
         (is (.contains test-out-string "test out"))
         (is (.contains test-out-string "<PASSED::>Test Passed<:LF:>"))))))
