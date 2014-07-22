756b6e5ad1a121a0c0ae6e4c1858dde8015e3a2b
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-21 18:00:23 -0600

    Factored out "solution-only" and "full-project" multi-methods

--- jvm-runner/src/clojure/codewars/runner.clj
+++ jvm-runner/src/clojure/codewars/runner.clj
@@ -1,13 +1,22 @@
 (ns codewars.runner)
 
-(defmulti run
-  (fn [{:keys [:language]}]
-    (if (contains? (methods run) language)
-      language
-      ::not-implemented)))
-
-(defmethod run ::not-implemented
+(defmulti solution-only :language)
+(defmulti full-project :language)
+(defmethod solution-only :default
   [{:keys [:language]}]
   (throw (IllegalArgumentException.
           (format "Language %s is not implemented"
                   (pr-str language)))))
+
+(defmethod full-project :default
+  [{:keys [:language]}]
+  (throw (IllegalArgumentException.
+          (format "Language %s is not implemented"
+                  (pr-str language)))))
+
+(defn run
+  "Run solution code or a test-fixture."
+  [opts]
+  (if (nil? (:fixture opts))
+    (solution-only opts)
+    (full-project opts)))
