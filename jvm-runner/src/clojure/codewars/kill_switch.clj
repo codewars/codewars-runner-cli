f2acad9acc1037789685d40b91206593e99e6b51
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-07-22 10:15:28 -0600

    Refactored thunk-timeout

--- jvm-runner/src/clojure/codewars/kill_switch.clj
+++ jvm-runner/src/clojure/codewars/kill_switch.clj
@@ -1,43 +1,28 @@
 (ns codewars.kill-switch
   (:import [java.util.concurrent TimeoutException ExecutionException TimeUnit FutureTask]))
 
-(def ^{:doc "Create a map of pretty keywords to ugly TimeUnits"}
-  uglify-time-unit
-  (into {} (for [[enum aliases] {TimeUnit/NANOSECONDS [:ns :nanoseconds]
-                                 TimeUnit/MICROSECONDS [:us :microseconds]
-                                 TimeUnit/MILLISECONDS [:ms :milliseconds]
-                                 TimeUnit/SECONDS [:s :sec :seconds]}
-                 alias aliases]
-             {alias enum})))
-
+;; Adapted from https://github.com/Raynes/clojail/blob/master/src/clojail/core.clj#L24
 (defn thunk-timeout
-  "Takes a function and an amount of time to wait for thse function to finish
-   executing. The sandbox can do this for you. unit is any of :ns, :us, :ms,
-   or :s which correspond to TimeUnit/NANOSECONDS, MICROSECONDS, MILLISECONDS,
-   and SECONDS respectively."
-  ([thunk ms]
-     (thunk-timeout thunk ms :ms nil)) ; Default to milliseconds, because that's pretty common.
-  ([thunk time unit]
-     (thunk-timeout thunk time unit nil))
-  ([thunk time unit tg]
-     (let [task (FutureTask. thunk)
-           thr (if tg (Thread. tg task) (Thread. task))]
-       (try
-         (.start thr)
-         (.get task time (or (uglify-time-unit unit) unit))
-         (catch TimeoutException e
-           (.cancel task true)
-           (.stop thr)
-           (throw (TimeoutException. "Execution timed out.")))
-         (catch ExecutionException e
-           (.cancel task true)
-           (.stop thr)
-           (throw (.getCause e)))
-         (catch Exception e
-           (.cancel task true)
-           (.stop thr)
-           (throw e))
-         (finally (when tg (.stop tg)))))))
+  "Takes a function and an amount of time to wait for the function to finish
+   executing."
+  [thunk ms]
+  (let [task (FutureTask. thunk)
+        thr (Thread. task)]
+    (try
+      (.start thr)
+      (.get task ms TimeUnit/MILLISECONDS)
+      (catch TimeoutException e
+        (.cancel task true)
+        (.stop thr)
+        (throw (TimeoutException. (str "Timeout: " ms " msecs"))))
+      (catch ExecutionException e
+        (.cancel task true)
+        (.stop thr)
+        (throw (.getCause e)))
+      (catch Exception e
+        (.cancel task true)
+        (.stop thr)
+        (throw e)))))
 
 
 (defmacro with-timeout
