e5e1b4a1eb5cad2ed49b6c206c6b59b7cbd95be5
jake hoffner <jake.hoffner@gmail.com>
2014-08-05 15:01:07 -0700

    fixed broken specs, added missing go install to docker file, fixed typescript

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 var exec = require('child_process').exec,
-    docker = require('./lib/docker');
+    docker = require('./lib/docker'),
+    clc = require('cli-color');
 
-exec("docker run --entrypoint mocha " + docker.taggedImage() + " test/*", function(err, stdout, stderr)
+// i.e. docker run -i --entrypoint mocha codewars/cli-runner:0.1.22 --recursive -t 5000
+exec("docker run -i --entrypoint mocha " + docker.taggedImage() + " --recursive -t 5000", function(err, stdout, stderr)
 {
-    console.log(stdout || stderr);
+    console.log(stdout);
+    console.log(clc.red(stderr));
 });
 
