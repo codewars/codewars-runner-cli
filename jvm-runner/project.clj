fd8d940700e92651488c98d88669cbb5f4f54abb
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-08-13 09:44:14 -0500

    - Switching "runner" to "runners" to mirror node.js - No more dynapath - Java compiler throws compilation errors now when failing    - Also introducing testing for this

--- jvm-runner/project.clj
+++ jvm-runner/project.clj
@@ -4,7 +4,6 @@
   :javac-target "1.8"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [org.tcrawley/dynapath "0.2.3"]
                  [junit/junit "4.11"]
                  [environ "0.5.0"]]
   :plugins [[lein-environ "0.5.0"]]
