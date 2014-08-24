40e7d8236df6a31d3b2ce861c5372cb4eac4517a
jake hoffner <jake.hoffner@gmail.com>
2014-08-24 16:39:56 -0700

    split docker file into multiple images. Can use node build [language | image name] to build image. "fund" image is having issues, Haskell installation is broken.

--- pull.js
+++ pull.js
@@ -4,10 +4,6 @@ var docker = require('./lib/docker'),
     config = require('./lib/config'),
     opts = require("nomnom")
         .options({
-            image: {
-                abbr: 'i',
-                help: 'The image to pull. No tag needed, it will automatically be appended. If empty the default will be used.'
-            },
             version: {
                 abbr: 'v',
                 flag: true,
@@ -20,11 +16,21 @@ var docker = require('./lib/docker'),
         .help('This utility will rebuild the docker image for the latest version')
         .parse();
 
-docker.pull(opts.image, function(err, stdout, stderr) {
-    console.log(JSON.stringify({
-        docker: stdout || stderr,
-        version: config.version,
-        image: docker.taggedImage()
-    }));
+var results = {version: config.version, runners: {}};
+
+docker.imageChain(opts._, function(name, image, next)
+{
+    console.log("Pulling " + image);
+    docker.pull(image, function(err, stdout, stderr) {
+        results.runners[name] = {
+            output: stdout || stderr,
+            image: image
+        }
+        next();
+    });
+}).run(function()
+{
+    console.log(JSON.stringify(results));
 });
+
 
