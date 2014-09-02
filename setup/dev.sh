9c9cdbd48ddae8fcfede636487ac59d809051534
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-09-02 13:51:18 -0600

    Merging

--- setup/dev.sh
+++ setup/dev.sh
@@ -5,9 +5,18 @@ cd /vagrant
 npm install
 
 echo "Install supervisor"
-sudo npm install supervisor -g
+npm install supervisor -g
+
+echo "Install docker"
+apt-get -y install docker
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+echo "Setup docker to start at boot"
+update-rc.d docker.io defaults
+echo "Add vagrant as a dockerer"
+usermod -a -G docker vagrant
 
 echo "Install htop"
 apt-get install htop
 
-node build
+#node build
