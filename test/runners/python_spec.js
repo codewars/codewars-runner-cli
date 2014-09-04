304d38dd6693eccd47750d3b77fcd2c923f704c4
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-09-03 20:19:14 -0600

    Python3 works now

--- test/runners/python_spec.js
+++ test/runners/python_spec.js
@@ -1,29 +1,14 @@
 var expect = require('chai').expect;
 var runner = require('../../lib/runners/python');
 
-describe('python runner', function () {
+describe('python runner2', function () {
+    this.timeout(600);
 
     describe('.run', function () {
         it('should handle basic code evaluation', function (done) {
-            runner.run({language: 'python', solution: 'from __future__ import print_function ; print(42)'}, function (buffer) {
+            runner.run({language: 'python', solution: 'import sys; sys.stdout.write("42")'}, function (buffer) {
                 console.log(buffer);
-                expect(buffer.stdout).to.equal('42\n');
-                done();
-            });
-        });
-        it('version should be either 2.7.6 or 3.4.1', function (done) {
-            runner.run({
-                language: 'python',
-                solution: [
-                    "from sys import version_info",
-                    "if version_info in [(2,7,6,'final',0), (3,4,1,'final',0)]:",
-                    "  print ('Version is apparently either 2.7.6 or 3.4.1')",
-                    "else:",
-                    "  print ('BAD VERSION: {0}'.format(version_info))"
-                ].join('\n')
-            }, function (buffer) {
-                console.log(buffer);
-                expect(buffer.stdout).to.equal('Version is apparently either 2.7.6 or 3.4.1\n');
+                expect(buffer.stdout).to.equal('42');
                 done();
             });
         });
@@ -35,9 +20,9 @@ describe('python runner', function () {
                     solution: 'a = 1',
                     fixture: 'test.expect(a == 1)',
                     testFramework: 'cw-2'
-                }, function (buffer) {
+                },
+                function (buffer) {
                     console.log(buffer);
-
                     expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
                     done();
                 });
@@ -48,7 +33,8 @@ describe('python runner', function () {
                     solution: 'a = 1',
                     fixture: 'test.assert_equals(a, 1)',
                     testFramework: 'cw-2'
-                }, function (buffer) {
+                },
+                function (buffer) {
                     console.log(buffer);
                     expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
                     done();
@@ -61,7 +47,8 @@ describe('python runner', function () {
                     setup: 'b = 2',
                     fixture: 'test.assert_equals(b, 2)',
                     testFramework: 'cw-2'
-                }, function (buffer) {
+                },
+                function (buffer) {
                     console.log(buffer);
                     expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
                     done();
@@ -73,7 +60,8 @@ describe('python runner', function () {
                     solution: 'a = 1',
                     fixture: 'test.expect(a == 2)',
                     testFramework: 'cw-2'
-                }, function (buffer) {
+                },
+                function (buffer) {
                     console.log(buffer);
                     expect(buffer.stdout).to.equal('<FAILED::>Value is not what was expected\n');
                     done();
@@ -83,12 +71,60 @@ describe('python runner', function () {
         it('should handle a failed assertion', function (done) {
             runner.run({language: 'python',
                     solution: 'a.fail()',
-                    testFramework: 'cw-2'
-                }, function (buffer) {
+                    testFramework: 'cw-2'},
+                function (buffer) {
                     console.log(buffer);
                     expect(buffer.stderr).to.not.contain('File ');
                     expect(buffer.stderr).to.not.contain(', line ');
                     expect(buffer.stderr).to.not.contain('most recent call last');
+                    done();
+                });
+        });
+    });
+    describe('unittest', function () {
+        it('should handle a basic assertion', function (done) {
+            runner.run({language: 'python',
+                    solution: 'a = 1',
+                    fixture: [
+                        'class Test(unittest.TestCase):',
+                        '  def test_assert(self):',
+                        '    self.assertEqual(a, 1)'
+                    ].join('\n'),
+                    testFramework: 'unittest'},
+                function (buffer) {
+                    console.log(buffer);
+                    expect(buffer.stdout).to.equal('<PASSED::>Test Passed\n');
+                    done();
+                });
+        });
+        it('should handle a failed assetion', function (done) {
+            runner.run({language: 'python',
+                    solution: 'a = 1',
+                    fixture: [
+                        'class Test(unittest.TestCase):',
+                        '  def test_assert(self):',
+                        '    self.assertEqual(a, 2, "test failed")'
+                    ].join('\n'),
+                    testFramework: 'unittest'},
+                function (buffer) {
+                    console.log(buffer);
+                    expect(buffer.stdout).to.contain('<FAILED::>');
+                    expect(buffer.stdout).to.contain('test failed');
+                    done();
+                });
+        });
+        it('should handle a failed assetion', function (done) {
+            runner.run({language: 'python',
+                    solution: 'a = 1',
+                    fixture: [
+                        'class Test(unittest.TestCase):',
+                        '  def test_assert(self):',
+                        '    raise Exception("exceptions are my favorite, I always throw them")'
+                    ].join('\n'),
+                    testFramework: 'unittest'},
+                function (buffer) {
+                    console.log(buffer);
+                    expect(buffer.stdout).to.equal('<ERROR::>Unhandled Exception: exceptions are my favorite, I always throw them\n');
                     done();
                 });
         });
@@ -99,16 +135,15 @@ describe('python runner', function () {
             runner.run({
                     language: 'python',
                     setup: [
-                        'from sys import stdout',
                         'import subprocess',
                         'import re',
+                        'import sys',
                         'mongod = subprocess.Popen("mongod", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)',
                         'is_waiting = re.compile(r".*waiting for connections on port 27017")',
                         'while True:',
                         '  l = mongod.stdout.readline().decode("utf-8")',
-                        '  m = is_waiting.match(l)',
-                        '  if m:',
-                        '    stdout.write(l)',
+                        '  if is_waiting.match(l):',
+                        '    sys.stdout.write(l + "\\n")',
                         '    break'
                     ].join('\n'),
                     solution: [
