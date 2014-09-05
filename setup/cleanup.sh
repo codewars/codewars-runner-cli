ecc44c432752f770fab085a8a952ca147599fe47
jake hoffner <jake.hoffner@gmail.com>
2014-09-04 22:48:42 -0700

    added additional cleanup

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -8,6 +8,7 @@ echo "Cleaning up..."
 docker rm $(docker ps -a -q)
 
 # delete old images that are taking up space.
+timeout 45 docker rmi $(docker images | grep "0.1." | awk "{print $3}")
 timeout 15 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 
 
