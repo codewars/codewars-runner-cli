07a743c4f3c641c20d4aad0b66da47e5f28f9547
Andreas Kristiansen <mail@ankr.dk>
2014-10-05 18:01:22 +0200

    Cleanup and adding comments

--- frameworks/php/CodeWars/TestCase.php
+++ frameworks/php/CodeWars/TestCase.php
@@ -4,10 +4,30 @@ namespace CodeWars;
 
 use PHPUnit_Framework_TestCase;
 
+/**
+ * TestCase
+ *
+ */
 class TestCase extends PHPUnit_Framework_TestCase {
 
-    public $userScriptVariables = [];
+    /**
+     * An array of all the variable defined in the users script.
+     * These will be extracted to be available within the test method.
+     *
+     * @var array
+     */
+    protected $userScriptVariables = [];
 
+    /**
+     * __construct
+     *
+     * Overwriting in order to fetch the user script variables.
+     *
+     * @param string $name
+     * @param array  $data
+     * @param string $dataName
+     * @return void
+     */
     public function __construct($name = null, array $data = array(), $dataName = '') {
         $this->userScriptVariables = $data['user_vars'];
         unset($data['user_vars']);
