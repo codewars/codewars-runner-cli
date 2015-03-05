98bdd2b2fb5cb443c71bc1ae16bd34816fabe25a
jhoffner <jake.hoffner@gmail.com>
2015-03-04 18:28:00 -0800

    cleaned up old code

--- test/runners/python3_spec.js
+++
@@ -1,9 +0,0 @@
-describe('python3', function () {
-    it('can evaluate exactly the same tests as the python2 runner', function (done) {
-        require('fs').readFile('test/runners/python_spec.js', 'utf8', function (error, data) {
-            if (error) throw error;
-            eval(data.split('python').join('python3'));
-            done();
-        });
-    });
-});
