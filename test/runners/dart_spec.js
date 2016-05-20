1270f3b1e3fa0d8799008fef1fd9907f6f6f18d4
Hugo Armstrong <hugo.armstrong@gmail.com>
2016-05-21 02:57:58 +1000

    Progress on #150
    Getting trolled by the tmp files a lot.

--- test/runners/dart_spec.js
+++ test/runners/dart_spec.js
@@ -3,14 +3,50 @@ const runner = require('../runner');
 
 describe('dart runner', function() {
   describe('.run', function() {
+    // Basic code run
     it('should handle basic code evaluation', function(done) {
       runner.run({
         language: 'dart',
-        code: `main() {print('Boom!');}`
+        code: `
+          main() {
+            print('Bam');
+          }
+        `
       }, function(buffer) {
-        expect(buffer.stdout).to.equal('Boom!');
+        expect(buffer.stdout).to.equal('Bam');
         done();
       });
     });
+
+    it('should handle invalid code', function(done) {
+      runner.run({
+        language: 'dart',
+        code: `
+          main() {
+            print('Bam') // Looks like a semi-colon is missing...
+          }
+        `
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Bam');
+        done();
+      });
+    });
+
+    it('should run async code', function(done) {
+      runner.run({
+        language: 'dart',
+        code: `
+          import 'dart:async';
+          main() async {
+            var str = await new Future.value('Bam');
+            print(str);
+          }
+        `
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Bam');
+        done();
+      });
+    });
+
   });
 });
