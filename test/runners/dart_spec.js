e817eb08897af5dd7e7838cca45d3e6f4f7ed8a2
Hugo Armstrong <hugo.armstrong@gmail.com>
2016-05-21 20:32:09 +1000

    Resolves #150
    Implements basic Dart language. Outstanding is the formatting for
    the test framework.

--- test/runners/dart_spec.js
+++ test/runners/dart_spec.js
@@ -13,7 +13,7 @@ describe('dart runner', function() {
           }
         `
       }, function(buffer) {
-        expect(buffer.stdout).to.equal('Bam');
+        expect(buffer.stdout).to.equal('Bam\n');
         done();
       });
     });
@@ -27,12 +27,12 @@ describe('dart runner', function() {
           }
         `
       }, function(buffer) {
-        expect(buffer.stdout).to.equal('Bam');
+        expect(buffer.stderr).to.contain('semicolon expected');
         done();
       });
     });
 
-    it('should run async code', function(done) {
+    it('should error when trying to add imports to the code section', function(done) {
       runner.run({
         language: 'dart',
         code: `
@@ -43,7 +43,54 @@ describe('dart runner', function() {
           }
         `
       }, function(buffer) {
-        expect(buffer.stdout).to.equal('Bam');
+        expect(buffer.stderr).to.contain(`unexpected token 'import'`);
+        done();
+      });
+    });
+
+    it('should allow imports inside setup', function(done) {
+      runner.run({
+        language: 'dart',
+        setup: `import 'dart:async';`,
+        code: `
+          main() async {
+            var str = await new Future.value('Bam');
+            print(str);
+          }
+        `
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain(`Bam\n`);
+        done();
+      });
+    });
+
+    it('should handle basic tests', function(done) {
+      runner.run({
+        language: 'dart',
+        code: `testFunction() => 50;`,
+        fixture: `
+          test('function returns 50', () {
+            expect(testFunction(), equals(50));
+          });`,
+        testFramework: 'test'
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain(`All tests passed`);
+        done();
+      });
+    });
+
+    it('should handle advanced tests with setup', function(done) {
+      runner.run({
+        language: 'dart',
+        setup: `import 'dart:async';`,
+        code: `testFunction() => new Future.value(50);`,
+        fixture: `
+          test('function returns 50', () async {
+            expect(await testFunction(), equals(50));
+          });`,
+        testFramework: 'test'
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain(`All tests passed`);
         done();
       });
     });
