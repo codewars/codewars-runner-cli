1ee4f5eff2758d9035bbb6a1b4e6f9625e59d848
RUSLAN LÓPEZ CARRO <scherzo_16@hotmail.com>
2016-06-01 12:50:02 -0500

    Can you please try this?
    Can't test it since Groovy runner seems to be down :cry:
    
    Tryed this on tutorialspoint site, sending a file with the fixture first and below the code.

--- jvm-runner/test/codewars/runners/groovy_test.clj
+++ jvm-runner/test/codewars/runners/groovy_test.clj
@@ -12,6 +12,17 @@
         :code "1 + 1"})
       (is (= 2 (-main))))))
 
+(deftest groovy-basic
+  (testing "-main can handle a very basic groovy code and fixture"
+    (with-in-str
+      (json/generate-string
+       {:language "groovy"
+        :code "class Greeter { static greet = { name -> 'Hello '+name } }"
+        :fixture "class MyTestCase extends GroovyTestCase { void testGreet(){def result = Greeter.greet('Ruslan');assert result == 'Hello Ruslan'}}"
+       }
+      )
+      (is (= org.junit.runner.Result (-main))))))
+
 (deftest groovy-java-out
   (testing "-main can handle a groovy code with no setup code but no fixture"
     (with-in-str
