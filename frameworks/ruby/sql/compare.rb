7091c2726bc74674b6ce67b101c993ef92669a18
jhoffner <jake.hoffner@gmail.com>
2016-10-24 19:08:15 -0700

    - Added compare DSL to SQL - RSpec now always delete the setup file so that possible solutions can't be read

--- frameworks/ruby/sql/compare.rb
+++ frameworks/ruby/sql/compare.rb
@@ -1,18 +1,30 @@
+def compare_with(expected, limit: 100, collapsed: true, &block)
+  sql_compare = SqlCompare.new(expected, limit: limit, collapsed: collapsed)
+  sql_compare.instance_eval(&block) if block
+
+  sql_compare.spec
+end
+
 class SqlCompare
   attr_reader :actual, :expected, :chart
-  def initialize(expected, chart: nil, limit: 100, collapsed: false)
-    @results = run_sql(label: '-Results: Actual', limit: limit, collapsed: collapsed)
+
+  def initialize(expected, chart: nil, limit: 100, collapsed: true)
+    @results = run_sql(label: 'Results: Actual', limit: limit, collapsed: collapsed)
     @actual = @results.to_a
     @expected = expected.to_a
     Display.table(expected.to_a.take(limit), label: 'Results: Expected', tab: true)
 
-    @chart = Hashie::Mash.new(chart)
-    draw_charts if @chart
+    draw_chart(chart) if chart
 
+    @column_blocks ||= {}
+  end
+
+  def draw_chart(config)
+    @chart = Hashie::Mash.new(config)
+    draw_charts
   end
 
   def column(name, &block)
-    @column_blocks ||= {}
     @column_blocks[name.to_sym] = block.to_proc
     self
   end
@@ -23,13 +35,19 @@ class SqlCompare
   end
 
   def spec(&block)
+    return if @spec_called
+    @spec_called = true
+
     _self = self
-    column_blocks = @column_blocks || {}
+    column_blocks = @column_blocks
     rows_block = @rows_block
+    it_blocks = @it_blocks
 
     RSpec.describe "Query Tests" do
       let(:actual) { _self.actual }
       let(:expected) { _self.expected }
+
+
 
       expected.first.each do |key, value|
         describe "\"#{key}\" column" do
