e2b51b32ca93fa0c29dc561ef30f2ad8a9f467d2
kazk <kazk.dev@gmail.com>
2017-03-16 19:37:23 -0700

    [Nim] Add test integration
    Implement CodewarsOutputFormatter and make use of the new custom test
    report formatter feature.  We need to use the latest Nim since this was
    merged few days ago.  Change back to a release tarball when a version
    with this feature becomes available.

---
+++ frameworks/nim/codewars/formatter.nim
@@ -0,0 +1,78 @@
+import strutils, times, unittest
+
+proc escapeLF(s: string): string = s.replace("\n", "<:LF:>")
+
+proc removePrefix(s: string): string =
+  # fixture.nim(8,20): Check failed: sub(1, 1) == 0
+  # -------------------
+  # remove assertion location because it's not useful on Codewars
+  return s[s.find(':')+2 .. s.high]
+
+type CodewarsOutputFormatter* = ref object of OutputFormatter
+  ## Custom output formatter for Codewars
+  suiteStartTime: float
+  testErrors: seq[string]
+  testStartTime: float
+  testStackTrace: string
+
+proc newCodewarsOutputFormatter*(): CodewarsOutputFormatter =
+  CodewarsOutputFormatter(
+    suiteStartTime: 0.0,
+    testErrors: @[],
+    testStackTrace: "",
+    testStartTime: 0.0
+  )
+
+method suiteStarted*(f: CodewarsOutputFormatter, suiteName: string) =
+  echo("<DESCRIBE::>" & escapeLF(suiteName))
+  f.suiteStartTime = epochTime()
+
+method testStarted*(f: CodewarsOutputFormatter, testName: string) =
+  echo("<IT::>" & escapeLF(testName))
+  f.testErrors.setLen(0)
+  f.testStackTrace.setLen(0)
+  f.testStartTime = epochTime()
+
+method failureOccurred*(f: CodewarsOutputFormatter, checkpoints: seq[string], stackTrace: string) =
+  f.testErrors.add(checkpoints)
+  if stackTrace != nil:
+    f.testStackTrace = stackTrace
+
+method testEnded*(f: CodewarsOutputFormatter, testResult: TestResult) =
+  let ms = (epochTime() - f.testStartTime)*1000
+  case testResult.status:
+  of OK:
+    echo("<PASSED::>OK")
+  of SKIPPED:
+    echo("<LOG::>Skipped")
+  of FAILED:
+    # TODO: stack trace. borrowing JUnitOutputFormatter's for now
+    let failureMsg = if f.testStackTrace.len > 0 and f.testErrors.len > 0:
+                       f.testErrors[^1]
+                     elif f.testErrors.len > 0:
+                       f.testErrors[0]
+                     else:
+                       "Unknown failure"
+    var errs = ""
+    if f.testErrors.len > 1:
+      let startIdx = if f.testStackTrace.len > 0: 0 else: 1
+      let endIdx   = if f.testStackTrace.len > 0: f.testErrors.len - 2 else: f.testErrors.len - 1
+      for i in startIdx..endIdx:
+        if errs.len > 0: errs.add("\n")
+        errs.add(f.testErrors[i])
+
+    if f.testStackTrace.len > 0:
+      echo("<ERROR::>Test Error")
+      if errs.len > 0:
+        echo("<LOG:ESC:Error>" & escapeLF(removePrefix(failureMsg & "\n" & errs)))
+      else:
+        echo("<LOG:ESC:Error>" & escapeLF(removePrefix(failureMsg)))
+      echo("<TAB::Stack Trace>" & escapeLF(f.testStackTrace))
+    else:
+      echo("<FAILED::>" & escapeLF(removePrefix(failureMsg)))
+      if errs.len > 0: echo("<LOG:ESC:Failure>" & escapeLF(errs))
+  echo("<COMPLETEDIN::>" & ms.formatFloat(ffDecimal, precision=4))
+
+method suiteEnded*(f: CodewarsOutputFormatter) =
+  let ms = (epochTime() - f.suiteStartTime)*1000
+  echo("<COMPLETEDIN::>", ms.formatFloat(ffDecimal, precision=4))
--- test/runners/nim_spec.js
+++ test/runners/nim_spec.js
@@ -1,8 +1,24 @@
 const expect = require('chai').expect;
 const runner = require('../runner');
+const exec = require('child_process').exec;
 
 describe('nim runner', function() {
-  it('should compile and run Nim', function(done) {
+  afterEach(function cleanup(done) {
+    exec([
+      'rm -rf',
+      '/home/codewarrior/nimcache',
+      '/home/codewarrior/solution',
+      '/home/codewarrior/fixture',
+      '/home/codewarrior/solution.txt',
+      '/home/codewarrior/solution.nim',
+      '/home/codewarrior/fixture.nim',
+    ].join(' '), function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  it('should compile and run Nim code', function(done) {
     runner.run({
       language: 'nim',
       solution: 'echo "42"'
@@ -11,4 +27,138 @@ describe('nim runner', function() {
       done();
     });
   });
+
+
+  it('should handle basic assertion', function(done) {
+    runner.run({
+      language: 'nim',
+      solution: [
+        'proc add*(x, y: int): int = x + y',
+      ].join('\n'),
+      fixture: [
+        'import unittest, codewars/formatter',
+        'import solution',
+        'addOutputFormatter(OutputFormatter(newCodewarsOutputFormatter()))',
+        '',
+        'suite "add":',
+        '  test "1 + 1 = 2":',
+        '    check(add(1, 1) == 2)',
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.contain('<PASSED::>');
+      done();
+    });
+  });
+
+  it('should handle basic assertion failure', function(done) {
+    runner.run({
+      language: 'nim',
+      solution: [
+        'proc add*(x, y: int): int = x - y',
+      ].join('\n'),
+      fixture: [
+        'import unittest, codewars/formatter',
+        'import solution',
+        'addOutputFormatter(OutputFormatter(newCodewarsOutputFormatter()))',
+        '',
+        'suite "add":',
+        '  test "1 + 1 = 2":',
+        '    check(add(1, 1) == 2)',
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.contain('<FAILED::>');
+      done();
+    });
+  });
+
+  it('should handle mixed success and failure', function(done) {
+    runner.run({
+      language: 'nim',
+      solution: [
+        'proc add*(x, y: int): int = x + y',
+        'proc sub*(x, y: int): int = x + y',
+      ].join('\n'),
+      fixture: [
+        'import unittest, codewars/formatter',
+        'import solution',
+        'addOutputFormatter(OutputFormatter(newCodewarsOutputFormatter()))',
+        '',
+        'suite "add/sub":',
+        '  test "1 + 1 = 2":',
+        '    check(add(1, 1) == 2)',
+        '  test "1 - 1 = 0":',
+        '    check(sub(1, 1) == 0)',
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.contain('<PASSED::>');
+      expect(buffer.stdout).to.contain('<FAILED::>');
+      done();
+    });
+  });
+
+  it('checks multiple assertions', function(done) {
+    runner.run({
+      language: 'nim',
+      solution: [
+        'proc add*(x, y: int): int = x + y',
+      ].join('\n'),
+      fixture: [
+        'import unittest, codewars/formatter',
+        'import solution',
+        'addOutputFormatter(OutputFormatter(newCodewarsOutputFormatter()))',
+        '',
+        'suite "add":',
+        '  test "1 + x":',
+        '    check:',
+        '      add(1, 1) == 2',
+        '      add(1, 2) == 3',
+        '      add(1, 3) == 4',
+        '      add(1, 4) == 5',
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.contain('<PASSED::>');
+      done();
+    });
+  });
+
+  it('expect macro ensures given Error is thrown', function(done) {
+    runner.run({
+      language: 'nim',
+      solution: [
+        'let v* = @[1, 2, 3]',
+      ].join('\n'),
+      fixture: [
+        'import unittest, codewars/formatter',
+        'import solution',
+        'addOutputFormatter(OutputFormatter(newCodewarsOutputFormatter()))',
+        '',
+        'suite "expect":',
+        '  test "out of bounds error is thrown on bad access":',
+        '    expect(IndexError):',
+        '      discard v[4]',
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.contain('<PASSED::>');
+      done();
+    });
+  });
 });
+
+describe('Examples', function() {
+  afterEach(function cleanup(done) {
+    exec([
+      'rm -rf',
+      '/home/codewarrior/nimcache',
+      '/home/codewarrior/solution',
+      '/home/codewarrior/fixture',
+      '/home/codewarrior/solution.txt',
+      '/home/codewarrior/solution.nim',
+      '/home/codewarrior/fixture.nim',
+    ].join(' '), function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+  runner.assertCodeExamples('nim');
+});
+
