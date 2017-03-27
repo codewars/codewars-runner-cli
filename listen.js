7ed06faf25c5bf2295764b5a463e6de60a51308d
kazk <kazk.dev@gmail.com>
2017-03-26 18:56:20 -0700

    [ESLint] Fix 'indent' *.js

--- listen.js
+++ listen.js
@@ -1,13 +1,13 @@
 // this file is used to keep a image alive so that it can be pre-warmed and communicated with.
 var net = require('net'),
-    fs = require('fs');
+  fs = require('fs');
 
 // Creates a new TCP server. The handler argument is automatically set as a listener for the 'connection' event
 var server = net.createServer(function (socket) {
 
     // Every time someone connects, tell them hello and then close the connection.
-    console.log("Connection from " + socket.remoteAddress);
-    socket.end("Hello World\n");
+  console.log("Connection from " + socket.remoteAddress);
+  socket.end("Hello World\n");
 
 });
 
