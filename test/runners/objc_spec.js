d26046b01f80a6a3dac3a041141c0911d65ff3a9
Ivano Picco <ivano.picco@pianobit.com>
2017-04-11 21:13:25 +0200

    objc: Minor fixes

--- test/runners/objc_spec.js
+++ test/runners/objc_spec.js
@@ -327,7 +327,7 @@ describe('objc runner', function() {
         #import <Foundation/Foundation.h>
           int main (int argc, const char * argv[]) {
             NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-            NSLog(@"Hello World");',
+            NSLog(@"Hello World");
             [pool drain];
           return 0;
           }`
