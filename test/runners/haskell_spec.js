570a05bb9dd430bbaf770fcc75c023a4c85225e1
kazk <kazk.dev@gmail.com>
2017-04-26 00:18:26 -0700

    [ESLint] Add options to 'indent' rule
     - Align declarations
     - Indent `case`

--- test/runners/haskell_spec.js
+++ test/runners/haskell_spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect,
-  runner = require('../runner');
+    runner = require('../runner');
 
 describe('haskell runner', function() {
   describe('.run', function() {
