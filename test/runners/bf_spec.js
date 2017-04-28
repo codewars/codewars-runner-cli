87fdecde7096f0cafd354e144c48eb96f5f1e6dd
DonaldKellett <i.donaldl@me.com>
2017-04-28 17:32:51 +0800

    Add Spec Tests for Brainf**k

---
+++ test/runners/bf_spec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect,
+    runner = require('../runner');
+
+describe("BF Runner", function () {
+  describe(".run", function () {
+    it('should handle basic code evaluation', function(done) {
+      runner.run({language: 'bf', code: '++++++++++[>+++>+++++++>+++++++++>++++++++++>+++++++++++<<<<<-]>>++.>>+.>--..+++.<<<<++.>>---.>>.+++.------.<-.<<<+.'}, function(buffer) {
+        expect(buffer.stdout).to.equal("Hello World!");
+        done();
+      });
+    });
+  });
+});
