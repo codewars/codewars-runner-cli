b43e92d8fca63ff0620ad8ca0d08d0897e82d1dc
DonaldKellett <i.donaldl@me.com>
2017-04-29 14:37:13 +0800

    Remove space between "function" and "()" to conform to ESLint rules

--- test/runners/bf_spec.js
+++ test/runners/bf_spec.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
 var runner = require('../runner');
 
-describe("BF Runner", function () {
-  describe("Basic Run", function () {
+describe("BF Runner", function() {
+  describe("Basic Run", function() {
     it('should handle basic code evaluation', function(done) {
       runner.run({language: 'bf', code: '++++++++++[>+++>+++++++>+++++++++>++++++++++>+++++++++++<<<<<-]>>++.>>+.>--..+++.<<<<++.>>---.>>.+++.------.<-.<<<+.'}, function(buffer) {
         expect(buffer.stdout).to.equal("Hello World!");
