94ac815ecf07f1489ac374a221d8f8b284f3107f
DonaldKellett <i.donaldl@me.com>
2017-05-04 10:54:49 +0800

    Convert Buffer to UTF8 String

--- frameworks/bf/run-bf.js
+++ frameworks/bf/run-bf.js
@@ -1,7 +1,7 @@
 var execSync = require('child_process').execSync;
 module.exports = function runBF(input) {
   if (typeof input != "undefined") {
-    return execSync('bf /workspace/solution.txt', {input})/* .replace(/^\<Buffer |\>$/g, "") */;
+    return execSync('bf /workspace/solution.txt', {input}).toString('utf8');
   }
-  return execSync('bf /workspace/solution.txt')/* .replace(/^\<Buffer |\>$/g, "") */;
+  return execSync('bf /workspace/solution.txt').toString('utf8');
 };
