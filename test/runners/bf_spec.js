0a33735959cf76f786f986897c012c4ecde9bd46
DonaldKellett <i.donaldl@me.com>
2017-05-05 14:54:40 +0800

    Remove more unnecessary backslashes

--- test/runners/bf_spec.js
+++ test/runners/bf_spec.js
@@ -10,41 +10,41 @@ describe("BF Runner", function() {
       });
     });
     it('should ignore anything not in "+-.,<>[]" as comments', function(done) {
-      runner.run({language: 'bf', code: "++++++++++ Initialize cell #0 to 10\
-[\
-  \"while\" loop begins\
-  >+++ Go to cell #1 and add 3\
-  >+++++++ Go to cell #2 and add 7\
-  >+++++++++ Go to cell #3 and add 9\
-  >++++++++++ Go to cell #4 and add 10\
-  >+++++++++++ Go to cell #5 and add 11\
-  <<<<<- Return to cell #0 and decrement its value\
-  \"while\" loop ends\
-]\
-[\
-  Now cell #0 has value 0,\
-  cell #1 has value 70,\
-  cell #2 has value 100,\
-  cell #3 has value 110,\
-  and cell #4 has value 30:\
-  0 | 70 | 100 | 110 | 30 | 0 | ...\
-  Note that this is what is known as a \"comment loop\".\
-  In a comment loop, all special characters in Brainfuck are ignored\
-  PROVIDED THAT: the value of the current cell is 0\
-  AND: all opening and closing square brackets \"[]\" are balanced\
-]\
->>++. Print \"H\"\
->>+. Print \"e\"\
->--. Print \"l\"\
-. Print \"l\"\
-+++. Print \"o\"\
-<<<<++. Print \" \" (spacebar character)\
->>---. Print \"W\"\
->>. Print \"o\"\
-+++. Print \"r\"\
-------. Print \"l\"\
-<-. Print \"d\"\
-<<<+. Print \"!\""}, function(buffer) {
+      runner.run({language: 'bf', code: `++++++++++ Initialize cell #0 to 10
+[
+  "while" loop begins
+  >+++ Go to cell #1 and add 3
+  >+++++++ Go to cell #2 and add 7
+  >+++++++++ Go to cell #3 and add 9
+  >++++++++++ Go to cell #4 and add 10
+  >+++++++++++ Go to cell #5 and add 11
+  <<<<<- Return to cell #0 and decrement its value
+  "while" loop ends
+]
+[
+  Now cell #0 has value 0,
+  cell #1 has value 70,
+  cell #2 has value 100,
+  cell #3 has value 110,
+  and cell #4 has value 30:
+  0 | 70 | 100 | 110 | 30 | 0 | ...
+  Note that this is what is known as a "comment loop".
+  In a comment loop, all special characters in Brainfuck are ignored
+  PROVIDED THAT: the value of the current cell is 0
+  AND: all opening and closing square brackets "[]" are balanced
+]
+>>++. Print "H"
+>>+. Print "e"
+>--. Print "l"
+. Print "l"
++++. Print "o"
+<<<<++. Print " " (spacebar character)
+>>---. Print "W"
+>>. Print "o"
++++. Print "r"
+------. Print "l"
+<-. Print "d"
+<<<+. Print "!"`}, function(buffer) {
         expect(buffer.stdout).to.equal("Hello World!");
         done();
       });
@@ -81,9 +81,9 @@ describe("BF Runner", function() {
       runner.run({
         language: 'bf',
         code: ',>,<[->[->>+<<]>>[-<+<+>>]<<<]>>.',
-        fixture: 'Test.assertEquals(runBF(String.fromCharCode(9, 8)), "H");\
-Test.assertEquals(runBF(String.fromCharCode(3, 5)), String.fromCharCode(15));\
-Test.assertEquals(runBF(String.fromCharCode(15, 12)), String.fromCharCode(180));',
+        fixture: `Test.assertEquals(runBF(String.fromCharCode(9, 8)), "H");
+Test.assertEquals(runBF(String.fromCharCode(3, 5)), String.fromCharCode(15));
+Test.assertEquals(runBF(String.fromCharCode(15, 12)), String.fromCharCode(180));`,
         testFramework: 'cw-2'
       }, function(buffer) {
         expect(buffer.stdout).to.contain('<PASSED::>');
