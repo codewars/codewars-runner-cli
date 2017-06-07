987875a6634bf7aa6c95e15bd6307f3b98361b57
kazk <kazk.dev@gmail.com>
2017-06-07 11:54:26 -0700

    [D] Produce executable file
     - Compile and execute in separate steps like C runner.
     - Add `-unittest` flag to enable including `unittest` blocks in code.
     - Add tests to show the output of built-in `unittest` on failure.

--- test/runners/d_spec.js
+++ test/runners/d_spec.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const expect = require('chai').expect;
 const runner = require('../runner');
 
@@ -5,15 +7,48 @@ describe('D runner', function() {
   it('should compile and run D', function(done) {
     runner.run({
       language: 'd',
-      solution: `
-import std.stdio;
-
-void main() {
-  writeln("42");
-}
-`
+      solution: [
+        `import std.stdio;`,
+        ``,
+        `void main() {`,
+        `  writeln("42");`,
+        `}`,
+      ].join('\n')
     }, function(buffer) {
       expect(buffer.stdout).to.equal('42\n');
+      done();
+    });
+  });
+
+  it('should compile and run D with passing unittest', function(done) {
+    runner.run({
+      language: 'd',
+      solution: [
+        `int add(int x, int y) { return x + y; }`,
+        `unittest {`,
+        `  assert(add(1, 1) == 2);`,
+        `}`,
+        `void main() {}`,
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.equal('');
+      done();
+    });
+  });
+
+  it('should compile and run D with failing unittest', function(done) {
+    runner.run({
+      language: 'd',
+      solution: [
+        `int add(int x, int y) { return x - y; }`,
+        `unittest {`,
+        `  assert(add(1, 1) == 2);`,
+        `}`,
+        `void main() {}`,
+      ].join('\n'),
+    }, function(buffer) {
+      expect(buffer.stdout).to.equal('');
+      expect(buffer.stderr).to.contain('core.exception.AssertError@code.d(3): unittest failure');
       done();
     });
   });
