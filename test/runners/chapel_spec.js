9cc09c4a8997a73f0fc710cc39c6da1ec7b25a62
SLx64 <lukasschulig@hotmail.de>
2017-06-27 21:42:19 +0200

    [Chapel] fixed indentation for ESLint

--- test/runners/chapel_spec.js
+++ test/runners/chapel_spec.js
@@ -14,52 +14,52 @@ describe('chapel runner', function() {
     });
   });
   describe('cw-2', function() {
-      it('should handle a basic assertion', function(done) {
-        runner.run({
-          language: 'chapel',
-          code: 'var a: int = 1;',
-          fixture: 'Test.expect(a == 1);',
-          testFramework: 'cw-2'
-        }, function(buffer) {
-          expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
-          done();
-        });
+    it('should handle a basic assertion', function(done) {
+      runner.run({
+        language: 'chapel',
+        code: 'var a: int = 1;',
+        fixture: 'Test.expect(a == 1);',
+        testFramework: 'cw-2'
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
+        done();
       });
-      it('should handle a basic assert_equals', function(done) {
-          runner.run({
-            language: 'chapel',
-            code: 'var a: int = 1;',
-            fixture: 'Test.assertEquals(a, 1);',
-            testFramework: 'cw-2'
-          }, function(buffer) {
-            expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
-            done();
-        });
+    });
+    it('should handle a basic assert_equals', function(done) {
+      runner.run({
+        language: 'chapel',
+        code: 'var a: int = 1;',
+        fixture: 'Test.assertEquals(a, 1);',
+        testFramework: 'cw-2'
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
+        done();
       });
-      it('should handle a basic setup', function(done) {
-        runner.run({
-          language: 'chapel',
-          code: 'var a: int = 1;',
-          setup: 'var b: int = 2;',
-          fixture: 'Test.assertEquals(b, 2);',
-          testFramework: 'cw-2'
-        }, function(buffer) {
-          expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
-          done();
-        });
+    });
+    it('should handle a basic setup', function(done) {
+      runner.run({
+        language: 'chapel',
+        code: 'var a: int = 1;',
+        setup: 'var b: int = 2;',
+        fixture: 'Test.assertEquals(b, 2);',
+        testFramework: 'cw-2'
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('\n<PASSED::>Test Passed\n');
+        done();
       });
-      it('should handle a failed assertion', function(done) {
-        runner.run({
-          language: 'chapel',
-          code: 'var a: int = 1;',
-          fixture: 'Test.expect(a == 2);',
-          testFramework: 'cw-2'
-        }, function(buffer) {
-          expect(buffer.stdout).to.equal('\n<FAILED::>Value was not what was expected\n');
-          done();
-        });
+    });
+    it('should handle a failed assertion', function(done) {
+      runner.run({
+        language: 'chapel',
+        code: 'var a: int = 1;',
+        fixture: 'Test.expect(a == 2);',
+        testFramework: 'cw-2'
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('\n<FAILED::>Value was not what was expected\n');
+        done();
       });
-   });
+    });
+  });
 });
 
 describe('Output format commands', function() {
