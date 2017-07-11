46e972a575b92a1605a95324692632f11e04e2f7
Phil DeJarnett <phil@overzealous.com>
2017-07-11 17:50:49 -0400

    Pinned the versions of Typings, Coffeescript, and TypeScript
    - Also reordered them to keep the two TS libraries together.
    - ESLinting

--- test/runners/typescript_spec.js
+++ test/runners/typescript_spec.js
@@ -7,7 +7,7 @@ describe('typescript runner', function() {
 
   describe('.run', function() {
     it('should handle basic code evaluation', function(done) {
-      runner.run({ language: 'typescript', code: 'console.log(42)' }, function(buffer) {
+      runner.run({language: 'typescript', code: 'console.log(42)'}, function(buffer) {
         expect(buffer.stdout).to.equal('42\n');
         done();
       });
