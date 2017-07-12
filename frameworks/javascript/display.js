38303c53124160213317e4b6b97e59b16dc0709c
jhoffner <jake.hoffner@gmail.com>
2017-07-12 16:16:41 -0700

    Code Cleanup - docker images no longer use timeout as default entrypoint - ESLint fixes

--- frameworks/javascript/display.js
+++ frameworks/javascript/display.js
@@ -134,11 +134,11 @@ module.exports.format = function format(obj, options) {
  */
 module.exports.escapeHtml = function escapeHtml(html) {
   return String(html)
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
 };
 
 /**
