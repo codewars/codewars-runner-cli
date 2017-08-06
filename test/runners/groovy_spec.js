00f466bad59da623267af6e878e19cd601494851
kazk <kazk.dev@gmail.com>
2017-08-06 11:43:21 -0700

    Add Groovy support
    Groovy 2.4.12
    
    Test Frameworks
    - JUnit 4 <http://junit.org/junit4/>
    - Spock <http://spockframework.org>

--- test/runners/groovy_spec.js
+++ test/runners/groovy_spec.js
@@ -1,14 +1,391 @@
-var expect = require('chai').expect;
-var runner = require('../runner');
+"use strict";
 
+const expect = require('chai').expect;
+const runner = require('../runner');
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const exec = require('child_process').exec;
 
-describe('groovy runner', function() {
-  describe('.run', function() {
-    it('should handle basic code evaluation', function(done) {
-      runner.run({language: 'groovy', code: "println '42'"}, function(buffer) {
-        expect(buffer.stdout).to.equal('42\n');
+describe('groovy-runner', function() {
+  before(function startDaemon(done) {
+    this.timeout(0);
+    if (process.env.GRADLE_DAEMON_FLAG !== '--daemon') {
+      console.log('Starting Gradle daemon');
+      exec('gradle --daemon test', {
+        cwd: '/runner/frameworks/gradle',
+      }, (err) => {
+        if (err) return done(err);
+        process.env.GRADLE_DAEMON_FLAG = '--daemon';
+        done();
+      });
+    }
+    else {
+      process.nextTick(done);
+    }
+  });
+
+  describe('running', function() {
+    afterEach(function cleanup(done) {
+      exec('rm -rf /home/codewarrior/src', function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should handle basic code evaluation (script)', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        solution: [
+          `println 'Hello World! from Groovy'`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Hello World! from Groovy\n');
+        done();
+      });
+    });
+
+    it('should handle basic code evaluation (static void main)', function(done) {
+      // class needs to be `Main`
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        solution: [
+          `class Main {`,
+          `  static void main(String... args) {`,
+          `    println 'Hello World! from Groovy'`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Hello World! from Groovy\n');
+        done();
+      });
+    });
+
+    it('should handle setup code', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        setup: [
+          `class Greeter {`,
+          `  static String greet(String name) {`,
+          '    return "Hello, ${name}"',
+          `  }`,
+          `}`,
+        ].join('\n'),
+        solution: [
+          `println Greeter.greet("Joe")`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Hello, Joe\n');
+        done();
+      });
+    });
+
+    it('should handle setup code with package declaration', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        setup: [
+          `package setup`,
+          `class Greeter {`,
+          `  static String greet(String name) {`,
+          '    return "Hello, ${name}"',
+          `  }`,
+          `}`,
+        ].join('\n'),
+        solution: [
+          `println setup.Greeter.greet("Joe")`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Hello, Joe\n');
+        done();
+      });
+    });
+
+    it('should handle setup code with arbitrary package declaration', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        setup: [
+          `package a.b.c.d.e`,
+          `class Greeter {`,
+          `  static String greet(String name) {`,
+          '    return "Hello, ${name}"',
+          `  }`,
+          `}`,
+        ].join('\n'),
+        solution: [
+          `println a.b.c.d.e.Greeter.greet("Joe")`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.equal('Hello, Joe\n');
         done();
       });
     });
   });
+
+  describe('testing with JUnit', function() {
+    afterEach(function cleanup(done) {
+      exec('rm -rf /home/codewarrior/src', function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should handle basic assertion', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion with package declared', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `package kata`,
+          ``,
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert kata.Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion failure', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a - b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<FAILED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion failure by error', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a / b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 0) == 1`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<ERROR::>');
+        done();
+      });
+    });
+
+
+    it('should handle basic assertion failure with message', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a - b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 1) == 2 : "add(1, 1) should return 2"`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<FAILED::>');
+        expect(buffer.stdout).to.contain('add(1, 1) should return 2');
+        done();
+      });
+    });
+
+
+    it('can have multiple suites', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAddPositives {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+          `class TestAddNegatives {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(-1, -1) == -2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        const expected = [
+          '<DESCRIBE::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '<COMPLETEDIN::>',
+          '<DESCRIBE::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '<COMPLETEDIN::>',
+        ].join('').replace(/\s/g, '');
+        expect(buffer.stdout.match(/<(?:DESCRIBE|IT|PASSED|FAILED|COMPLETEDIN)::>/g).join('')).to.equal(expected);
+        done();
+      });
+    });
+
+
+    it('should allow logging', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'junit4',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) {`,
+          '    println "a = ${a}, b = ${b}"',
+          `    return a + b`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import org.junit.Test`,
+          ``,
+          `class TestAdd {`,
+          `  @Test`,
+          `  void addTest() {`,
+          `    assert Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('a = 1, b = 1');
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+  });
+
+  describe.skip('testing with Spock', function() {
+    // TODO Add tests for Spock
+  });
+
+  describe('Example Challenges', function() {
+    afterEach(function cleanup(done) {
+      exec('rm -rf /home/codewarrior/src', function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    forEachExamples(function(framework, name, example) {
+      describe(`${framework}: "${name}" example`, function() {
+        it('should define an initial code block', function() {
+          expect(example.initial).to.be.a('string');
+        });
+
+        it('should have a passing solution', function(done) {
+          this.timeout(0);
+          runner.run({
+            language: 'groovy',
+            setup: example.setup,
+            solution: example.answer,
+            fixture: example.fixture,
+            testFramework: framework,
+          }, function(buffer) {
+            expect(buffer.stdout).to.contain('<PASSED::>');
+            expect(buffer.stdout).to.not.contain('<FAILED::>');
+            expect(buffer.stdout).to.not.contain('<ERROR::>');
+            done();
+          });
+        });
+      });
+    });
+  });
 });
+
+
+function forEachExamples(cb) {
+  const examples = yaml.safeLoad(fs.readFileSync(path.join(__dirname, `../../examples/groovy.yml`), 'utf8'));
+  for (const framework of Object.keys(examples)) {
+    for (const example of Object.keys(examples[framework])) {
+      cb(framework, example, examples[framework][example]);
+    }
+  }
+}
