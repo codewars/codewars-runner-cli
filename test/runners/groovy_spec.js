915360988c354d22d31069230c1a8b2a0930845c
kazk <kazk.dev@gmail.com>
2017-08-10 16:25:42 -0700

    Add tests and examples for Spock

--- test/runners/groovy_spec.js
+++ test/runners/groovy_spec.js
@@ -336,8 +336,235 @@ describe('groovy-runner', function() {
     });
   });
 
-  describe.skip('testing with Spock', function() {
-    // TODO Add tests for Spock
+  describe('testing with Spock', function() {
+    afterEach(function cleanup(done) {
+      exec('rm -rf /home/codewarrior/project', function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should handle basic assertion', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion with package declared', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `package kata`,
+          ``,
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    kata.Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion failure', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a - b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<FAILED::>');
+        done();
+      });
+    });
+
+    it('should handle basic assertion failure by error', function(done) { // No <ERROR::>
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a / b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(1, 0) == 1`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).not.to.contain('<ERROR::>');
+        expect(buffer.stdout).to.contain('<FAILED::>');
+        expect(buffer.stdout).to.contain('java.lang.ArithmeticException: Division by zero');
+        done();
+      });
+    });
+
+    it('can have multiple suites', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec1 extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+          `class AdderSpec2 extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(-1, -1) == -2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        const expected = [
+          '<DESCRIBE::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '<COMPLETEDIN::>',
+          '<DESCRIBE::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '<COMPLETEDIN::>',
+        ].join('').replace(/\s/g, '');
+        expect(buffer.stdout.match(/<(?:DESCRIBE|IT|PASSED|FAILED|COMPLETEDIN)::>/g).join('')).to.equal(expected);
+        done();
+      });
+    });
+
+    it('should allow logging', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) {`,
+          '    println "a = ${a}, b = ${b}"',
+          `    return a + b`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  def "Adder.add returns the sum"() {`,
+          `    expect:`,
+          `    Adder.add(1, 1) == 2`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('a = 1, b = 1');
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        done();
+      });
+    });
+
+    it('should support @Unroll', function(done) {
+      this.timeout(0);
+      runner.run({
+        language: 'groovy',
+        testFramework: 'spock',
+        solution: [
+          `class Adder {`,
+          `  static def add(a, b) { return a + b }`,
+          `}`,
+        ].join('\n'),
+        fixture: [
+          `import spock.lang.Specification`,
+          `import spock.lang.Unroll`,
+          ``,
+          `class AdderSpec extends Specification {`,
+          `  @Unroll`,
+          `  def "Adder.add(#a, #b) returns the sum #c"() {`,
+          `    expect:`,
+          `    Adder.add(a, b) == c`,
+          ``,
+          `    where:`,
+          `    a | b || c`,
+          `    1 | 1 || 2`,
+          `    1 | 2 || 3`,
+          `    1 | 3 || 4`,
+          `    1 | 4 || 5`,
+          `  }`,
+          `}`,
+        ].join('\n'),
+      }, function(buffer) {
+        expect(buffer.stdout).to.contain('<PASSED::>');
+        const expected = [
+          '<DESCRIBE::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '  <IT::><PASSED::><COMPLETEDIN::>',
+          '<COMPLETEDIN::>',
+        ].join('').replace(/\s/g, '');
+        expect(buffer.stdout.match(/<(?:DESCRIBE|IT|PASSED|FAILED|COMPLETEDIN)::>/g).join('')).to.equal(expected);
+        done();
+      });
+    });
   });
 
   describe('Example Challenges', function() {
