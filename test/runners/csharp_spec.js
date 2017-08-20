3789cbb22563864d2b1df4757e93109f5c4b7526
kazk <kazk.dev@gmail.com>
2017-08-20 14:24:44 -0700

    Make csharp_spec.js readable

--- test/runners/csharp_spec.js
+++ test/runners/csharp_spec.js
@@ -1,5 +1,8 @@
-var expect = require('chai').expect,
-    runner = require('../runner');
+"use strict";
+
+const expect = require('chai').expect;
+
+const runner = require('../runner');
 
 describe('c# runner', function() {
   describe('.run', function() {
@@ -8,7 +11,13 @@ describe('c# runner', function() {
     it('should handle basic code evaluation', function(done) {
       runner.run({
         language: 'csharp',
-        code: 'public class Hello1 { public static void Main() { System.Console.WriteLine("Hello, World!");}}'
+        solution: [
+          'public class Hello1 {',
+          '  public static void Main() {',
+          '    System.Console.WriteLine("Hello, World!");',
+          '  }',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         //console.log(buffer);
         expect(buffer.stdout).to.contain('Hello, World!\n');
@@ -19,8 +28,18 @@ describe('c# runner', function() {
     it('should handle setup', function(done) {
       runner.run({
         language: 'csharp',
-        setup: 'public class Preloaded { public static string hello = "Hello, World!"; }',
-        code: 'public class Hello1 { public static void Main() { System.Console.WriteLine(Preloaded.hello);}}'
+        setup: [
+          'public class Preloaded {',
+          '  public static string hello = "Hello, World!";',
+          '}',
+        ].join('\n'),
+        solution: [
+          'public class Hello1 {',
+          '  public static void Main() {',
+          '    System.Console.WriteLine(Preloaded.hello);',
+          '  }',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         expect(buffer.stdout).to.contain('Hello, World!\n');
         done();
@@ -30,8 +49,52 @@ describe('c# runner', function() {
     it('should handle basic nunit tests', function(done) {
       runner.run({
         language: 'csharp',
-        code: 'namespace Bank { using System; public class Account { private decimal balance; public void Deposit(decimal amount) { Console.WriteLine("slorgs"); balance += amount; } public void Withdraw(decimal amount) { balance -= amount; } public void TransferFunds(Account destination, decimal amount) { } public decimal Balance { get { return balance; } } } } ',
-        fixture: 'namespace Bank { using NUnit.Framework; [TestFixture] public class AccountTest { [Test] public void TransferFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); source.TransferFunds(destination, 100m); Assert.AreEqual(250m, destination.Balance); Assert.AreEqual(100m, source.Balance); } [Test] public void CheckFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); Assert.AreEqual(200m, source.Balance); } } } '
+        solution: [
+          'namespace Bank {',
+          '  using System;',
+          '  public class Account {',
+          '    private decimal balance;',
+          '    public void Deposit(decimal amount) {',
+          '      Console.WriteLine("slorgs");',
+          '      balance += amount;',
+          '    }',
+          '    public void Withdraw(decimal amount) {',
+          '      balance -= amount;',
+          '    }',
+          '    public void TransferFunds(Account destination, decimal amount) {',
+          '    }',
+          '    public decimal Balance {',
+          '      get { return balance; }',
+          '    }',
+          '  }',
+          '} ',
+        ].join('\n'),
+        fixture: [
+          'namespace Bank {',
+          '  using NUnit.Framework;',
+          '  [TestFixture]',
+          '  public class AccountTest {',
+          '    [Test]',
+          '    public void TransferFunds() {',
+          '      Account source = new Account();',
+          '      source.Deposit(200m);',
+          '      Account destination = new Account();',
+          '      destination.Deposit(150m);',
+          '      source.TransferFunds(destination, 100m);',
+          '      Assert.AreEqual(250m, destination.Balance);',
+          '      Assert.AreEqual(100m, source.Balance);',
+          '    }',
+          '    [Test]',
+          '    public void CheckFunds() {',
+          '      Account source = new Account();',
+          '      source.Deposit(200m);',
+          '      Account destination = new Account();',
+          '      destination.Deposit(150m);',
+          '      Assert.AreEqual(200m, source.Balance);',
+          '    }',
+          '  }',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         //console.log(buffer);
         expect(buffer.stdout).to.contain("slorgs");
@@ -46,8 +109,50 @@ describe('c# runner', function() {
     it('should handle bad code', function(done) {
       runner.run({
         language: 'csharp',
-        code: 'namespace Bank { using System; using System.Drawing; public class Account { private decimal balance; public void Deposit(decimal amount) { Console.WriteLine("slorgs"); balance += amount; } public void Withdraw(decimal amount) { balance -= Amount; } public void TransferFunds(Account destination, decimal amount) { } public decimal Balance { get { return balance; } } } } ',
-        fixture: 'namespace Bank { using NUnit.Framework; [TestFixture] public class AccountTest { [Test] public void TransferFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); source.TransferFunds(destination, 100m); Assert.AreEqual(250m, destination.Balance); Assert.AreEqual(100m, source.Balance); } [Test] public void CheckFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); Assert.AreEqual(200m, source.Balance); } } } '
+        solution: [
+          'namespace Bank {',
+          '  using System;',
+          '  using System.Drawing;',
+          '  public class Account {',
+          '    private decimal balance;',
+          '    public void Deposit(decimal amount) {',
+          '      Console.WriteLine("slorgs");',
+          '      balance += amount;',
+          '    }',
+          '    public void Withdraw(decimal amount) {',
+          '      balance -= Amount;',
+          '    }',
+          '    public void TransferFunds(Account destination, decimal amount) { }',
+          '    public decimal Balance { get { return balance; } }',
+          '  }',
+          '}',
+        ].join('\n'),
+        fixture: [
+          'namespace Bank {',
+          '  using NUnit.Framework;',
+          '  [TestFixture]',
+          '  public class AccountTest {',
+          '    [Test]',
+          '    public void TransferFunds() {',
+          '      Account source = new Account();',
+          '      source.Deposit(200m);',
+          '      Account destination = new Account();',
+          '      destination.Deposit(150m);',
+          '      source.TransferFunds(destination, 100m);',
+          '      Assert.AreEqual(250m, destination.Balance);',
+          '      Assert.AreEqual(100m, source.Balance);',
+          '    }',
+          '    [Test]',
+          '    public void CheckFunds() {',
+          '      Account source = new Account();',
+          '      source.Deposit(200m);',
+          '      Account destination = new Account();',
+          '      destination.Deposit(150m);',
+          '      Assert.AreEqual(200m, source.Balance);',
+          '    }',
+          '  }',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         console.log(buffer);
         expect(buffer.stdout).to.not.contain("lib/runners/csharp.js");
@@ -60,7 +165,12 @@ describe('c# runner', function() {
       runner.run({
         language: 'csharp',
         format: 'json',
-        code: 'public class CheckChoose{ public static void Main(){} public static long Checkchoose(long m, int n){}}'
+        solution: [
+          'public class CheckChoose {',
+          '  public static void Main() {}',
+          '  public static long Checkchoose(long m, int n) {}',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         // console.log(buffer);
         console.log('');
@@ -74,8 +184,25 @@ describe('c# runner', function() {
     it('should handle partially passed code', function(done) {
       runner.run({
         language: 'csharp',
-        code: 'using System; public class Account { } ',
-        fixture: 'using NUnit.Framework; [TestFixture] public class AccountTest { [Test] public void A() { Assert.AreEqual(1, 1); } [Test] public void Z() { Assert.AreEqual(1, 1); throw new System.Exception(); } } '
+        solution: [
+          'using System;',
+          'public class Account { }',
+        ].join('\n'),
+        fixture: [
+          'using NUnit.Framework;',
+          '[TestFixture]',
+          'public class AccountTest {',
+          '  [Test]',
+          '  public void A() {',
+          '    Assert.AreEqual(1, 1);',
+          '  }',
+          '  [Test]',
+          '  public void Z() {',
+          '    Assert.AreEqual(1, 1);',
+          '    throw new System.Exception();',
+          '  }',
+          '}',
+        ].join('\n'),
       }, function(buffer) {
         expect(buffer.stdout).to.contain("<ERROR::>");
         done();
@@ -88,8 +215,47 @@ describe('c# runner', function() {
         // references: ['SkiaSharp.dll'],
         files: {
           'foo.sh': 'test',
-          'solution.cs': 'namespace Bank { using System; public class Account { private decimal balance; public void Deposit(decimal amount) { Console.WriteLine("slorgs"); balance += amount; } public void Withdraw(decimal amount) { balance -= amount; } public void TransferFunds(Account destination, decimal amount) { } public decimal Balance { get { return balance; } } } } ',
-          'spec.cs': 'namespace Bank { using NUnit.Framework; [TestFixture] public class AccountTest { [Test] public void TransferFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); source.TransferFunds(destination, 100m); Assert.AreEqual(250m, destination.Balance); Assert.AreEqual(100m, source.Balance); } [Test] public void CheckFunds() { Account source = new Account(); source.Deposit(200m); Account destination = new Account(); destination.Deposit(150m); Assert.AreEqual(200m, source.Balance); } } } '
+          'solution.cs': [
+            'namespace Bank {',
+            '  using System;',
+            '  public class Account {',
+            '    private decimal balance;',
+            '    public void Deposit(decimal amount) {',
+            '      Console.WriteLine("slorgs");',
+            '      balance += amount;',
+            '    }',
+            '    public void Withdraw(decimal amount) { balance -= amount; }',
+            '    public void TransferFunds(Account destination, decimal amount) { }',
+            '    public decimal Balance { get { return balance; } }',
+            '  }',
+            '}',
+          ].join('\n'),
+          'spec.cs': [
+            'namespace Bank {',
+            '  using NUnit.Framework;',
+            '  [TestFixture]',
+            '  public class AccountTest {',
+            '    [Test]',
+            '    public void TransferFunds() {',
+            '      Account source = new Account();',
+            '      source.Deposit(200m);',
+            '      Account destination = new Account();',
+            '      destination.Deposit(150m);',
+            '      source.TransferFunds(destination, 100m);',
+            '      Assert.AreEqual(250m, destination.Balance);',
+            '      Assert.AreEqual(100m, source.Balance);',
+            '    }',
+            '    [Test]',
+            '    public void CheckFunds() {',
+            '      Account source = new Account();',
+            '      source.Deposit(200m);',
+            '      Account destination = new Account();',
+            '      destination.Deposit(150m);',
+            '      Assert.AreEqual(200m, source.Balance);',
+            '    }',
+            '  }',
+            '}',
+          ].join('\n'),
         }
       }, function(buffer) {
         //console.log(buffer);
