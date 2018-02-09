022a9779ebc1eee470aa98edfbe43f1980328acc
Bubbler-4 <bubbler9903@gmail.com>
2018-02-10 07:30:32 +0900

    Change Describe and It into decorators, and add timeout utility
    * Build the decorator version of `describe` and `it`, so the text fixture may look like this (and the decorator itself runs the code, so it doesn't need a separate runner function):
    
    ```python
    @describe('describe-text')
    def describe1():
        @it('it-text', before=f1, after=f2)
        def it1():
            # some test function
        @it('it-text')
        def it2():
            # some test function
    ```
    
    * Properly close `describe` and `it` blocks in the test output
    * Print the running times of `describe` and `it` blocks
    * Provide `before` and `after` for `describe` and `it`

--- frameworks/python/cw-2.py
+++ frameworks/python/cw-2.py
@@ -64,9 +64,54 @@ def assert_approx_equals(actual, expected, margin=1e-9, message=None, allow_rais
     expect(abs((actual - expected) / div) < margin, message, allow_raise)
 
 
-def describe(message):
-    display('DESCRIBE', message)
+'''
+Usage:
+@describe('describe text')
+def describe1():
+    @it('it text')
+    def it1():
+        # some test cases...
+'''
+def _timed_block_factory(opening_text):
+    from timeit import default_timer as timer
+    from traceback import format_exception
+    from sys import exc_info
+    
+    def _timed_block_decorator(s, before=None, after=None):
+        display(opening_text, s)
+        def wrapper(func):
+            if callable(before): before()
+            time = timer()
+            try: func()
+            except:
+                fail('Unexpected exception raised')
+                tb_str = ''.join(format_exception(*exc_info()))
+                display('ERROR', tb_str)
+            display('COMPLETEDIN', '{:.2f}'.format((timer() - time) * 1000))
+            if callable(after): after()
+        return wrapper
+    return _timed_block_decorator
+
+describe = _timed_block_factory('DESCRIBE')
+it = _timed_block_factory('IT')
 
 
-def it(message):
-    display('IT', message)
+'''
+Timeout utility
+Usage:
+@timeout(sec)
+def some_tests():
+    any code block...
+Note: Timeout value can be a float.
+'''
+def timeout(sec):
+    def wrapper(func):
+        from multiprocessing import Process
+        process = Process(target=func)
+        process.start()
+        process.join(sec)
+        if process.is_alive():
+            fail('Exceeded time limit of {:.3f} seconds'.format(sec))
+            process.terminate()
+            process.join()
+    return wrapper
